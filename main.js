e7bee3434e779f45e4cd2bd74aafc9cea3782d6d
Narou <narousou@gmail.com>
2025-11-05 18:28:14 -0500

    a

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 cardlocal =  document.getElementById("cards");
 function delta()
 {
-   let t = document.createElement("IMG");
-   cardlocal.appendElement(t);
+    let t = document.createElement("IMG");
+    t.setAttribute("width" ,w);
+    t.setAttribute("height",h);
+    t.setAttribute("src", "major_arcana/the_paradox.png");
+    cardlocal.appendElement(t);
 }
